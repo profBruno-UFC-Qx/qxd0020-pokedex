825399b2caaf2dccbc1f16c8d7830bf43ffdb692
Bruno Mateus <brunomateus@gmail.com>
2021-11-30 15:23:52 -0300

    Adding route to see more detail of a specific pokemon

--- routes/pokemons.ts
+++ routes/pokemons.ts
@@ -51,4 +51,54 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
 
+    const pokemonId = Number(req.params.id);
+
+    if(pokemonId < 0 || pokemonId > pokemons.length) {
+        res.status(404).send("Pokemon Not Found");
+    }
+
+    const pokemon = pokemons[pokemonId - 1];
+    
+    const content =`<tr>
+        <td>${pokemon.id}</td>
+        <td><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"><br>${pokemon.nome}</td>
+        <td>${pokemon.pvMax}</td>
+        <td>${pokemon.ataque}</td>
+        <td>${pokemon.defesa}</td>
+        <td>${pokemon.ataqueEspecial}</td>
+        <td>${pokemon.defesaEspecial}</td>
+        <td>${pokemon.velocidade}</td>
+        <td>${pokemon.peso}</td>
+        <td>${pokemon.altura}</td>
+    </tr>`;
+    
+    
+    res.status(200).send(
+        `<html>
+            <body>
+                <table>
+                    <caption>Pokedex</caption>
+                    <thead>
+                        <tr>
+                            <th> Id </th>
+                            <th> Nome </th>
+                            <th> PvMax </th>
+                            <th> Ataque </th>
+                            <th> Defesa </th>
+                            <th> Ataque Especial </th>
+                            <th> Defesa Especial </th>
+                            <th> Velocidade </th>
+                            <th> Peso </th>
+                            <th> Altura </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    ${content}
+                    </tbody>
+                </table>
+            </body>
+        </html>
+    `);
+
+
 });
