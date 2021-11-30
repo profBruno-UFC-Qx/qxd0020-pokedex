55bd002a8f041628e12a1610f3ebab6a6271a7cb
Bruno Mateus <brunomateus@gmail.com>
2021-11-30 14:46:57 -0300

    Pokedex equal to v1 but now using Express

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 The idea of this repo is to evolve a Pokedex starting with pure Node.js + Typescript
 to a final version using Node.js, TypeScript, Vue and Mongodb.
 
-- [v1: Pure Node + TypeScript](../../commit/ab31765a94179c766ecb0b62b190216a48fde05d):
+- [v1: Pure Node + TypeScript](../../commit/d7ac067b7ae0e9a444ff8fc35e91f2cd86a1bd7d):
 ![v1 Node + TS](img/pokedex-v1.png)
--- app.ts
+++ app.ts
@@ -1,55 +1,56 @@
-import { pokemons } from "./models/pokemon"
-import * as http from "http";
+import { pokemons } from "./models/pokemon";
+import express from 'express';
 import { AddressInfo } from "net";
 
-const server = http.createServer((req, res) => {
-    console.log(`A resquest ${req.method} arrived to ${req.url}`);
+const app = express();
+const PORT = 8080;
 
-    if (req.method === "GET") {
-        if(req.url === "/pokemons") {
-            let content = "";
+app.get('/', (req, res) => {
 
-            for (const pokemon of pokemons) {
-                content +=`<tr>
-                    <td>${pokemon.id}</td>
-                    <td><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"><br>${pokemon.nome}</td>
-                    <td>${pokemon.pvMax}</td>
-                    <td>${pokemon.ataque}</td>
-                    <td>${pokemon.defesa}</td>
-                    <td>${pokemon.ataqueEspecial}</td>
-                    <td>${pokemon.defesaEspecial}</td>
-                    <td>${pokemon.velocidade}</td>
-                </tr>`;
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(
-                `<html>
-                    <body>
-                        <table>
-                            <caption>Pokedex</caption>
-                            <thead>
-                                <tr>
-                                    <th> Id </th>
-                                    <th> Nome </th>
-                                    <th> PvMax </th>
-                                    <th> Ataque </th>
-                                    <th> Defesa </th>
-                                    <th> Ataque Especial </th>
-                                    <th> Defesa Especial </th>
-                                    <th> Velocidade </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            ${content}
-                            </tbody>
-                        </table>
-                    </body>
-                </html>
-            `);
-        }
+    let content = "";
+
+    for (const pokemon of pokemons) {
+        content +=`<tr>
+            <td>${pokemon.id}</td>
+            <td><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"><br>${pokemon.nome}</td>
+            <td>${pokemon.pvMax}</td>
+            <td>${pokemon.ataque}</td>
+            <td>${pokemon.defesa}</td>
+            <td>${pokemon.ataqueEspecial}</td>
+            <td>${pokemon.defesaEspecial}</td>
+            <td>${pokemon.velocidade}</td>
+            <td><a href="pokemons/${pokemon.id}">Ver detalhes</td>
+        </tr>`;
     }
+    
+    res.send(
+        `<html>
+            <body>
+                <table>
+                    <caption>Pokedex</caption>
+                    <thead>
+                        <tr>
+                            <th> Id </th>
+                            <th> Nome </th>
+                            <th> PvMax </th>
+                            <th> Ataque </th>
+                            <th> Defesa </th>
+                            <th> Ataque Especial </th>
+                            <th> Defesa Especial </th>
+                            <th> Velocidade </th>
+                            <th> Mais detalhes </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    ${content}
+                    </tbody>
+                </table>
+            </body>
+        </html>
+    `);
+    
 });
-server.listen(8080, "127.0.0.1", () => {
-    const { address, port } = server.address() as AddressInfo;
-    console.log(`Server listening on http://${address}:${port} `)
+
+app.listen(8080, () => {
+    console.log(`Server listening on http://localhost:${PORT} `)
 });
