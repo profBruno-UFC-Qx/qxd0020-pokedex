35c22fb1c0e50758551555b010671e645428f65a
Bruno Mateus <brunomateus@gmail.com>
2021-11-30 15:03:02 -0300

    using routes

--- app.ts
+++ app.ts
@@ -1,54 +1,14 @@
-import { pokemons } from "./models/pokemon";
 import express from 'express';
-import { AddressInfo } from "net";
+import { router as pokemonRouter } from './routes/pokemons';
 
 const app = express();
 const PORT = 8080;
 
+app.use('/pokemons', pokemonRouter);
+
+
 app.get('/', (req, res) => {
-
-    let content = "";
-
-    for (const pokemon of pokemons) {
-        content +=`<tr>
-            <td>${pokemon.id}</td>
-            <td><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"><br>${pokemon.nome}</td>
-            <td>${pokemon.pvMax}</td>
-            <td>${pokemon.ataque}</td>
-            <td>${pokemon.defesa}</td>
-            <td>${pokemon.ataqueEspecial}</td>
-            <td>${pokemon.defesaEspecial}</td>
-            <td>${pokemon.velocidade}</td>
-            <td><a href="pokemons/${pokemon.id}">Ver detalhes</td>
-        </tr>`;
-    }
-    
-    res.send(
-        `<html>
-            <body>
-                <table>
-                    <caption>Pokedex</caption>
-                    <thead>
-                        <tr>
-                            <th> Id </th>
-                            <th> Nome </th>
-                            <th> PvMax </th>
-                            <th> Ataque </th>
-                            <th> Defesa </th>
-                            <th> Ataque Especial </th>
-                            <th> Defesa Especial </th>
-                            <th> Velocidade </th>
-                            <th> Mais detalhes </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    ${content}
-                    </tbody>
-                </table>
-            </body>
-        </html>
-    `);
-    
+    res.redirect('/pokemons');
 });
 
 app.listen(8080, () => {
---
+++ routes/pokemons.ts
@@ -0,0 +1,54 @@
+import express from 'express';
+import { pokemons } from "../models/pokemon";
+
+export const router = express.Router();
+
+router.get('/', (req, res) => {
+
+    let content = "";
+
+    for (const pokemon of pokemons) {
+        content +=`<tr>
+            <td>${pokemon.id}</td>
+            <td><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"><br>${pokemon.nome}</td>
+            <td>${pokemon.pvMax}</td>
+            <td>${pokemon.ataque}</td>
+            <td>${pokemon.defesa}</td>
+            <td>${pokemon.ataqueEspecial}</td>
+            <td>${pokemon.defesaEspecial}</td>
+            <td>${pokemon.velocidade}</td>
+            <td><a href="pokemons/${pokemon.id}">Ver detalhes</td>
+        </tr>`;
+    }
+    
+    res.send(
+        `<html>
+            <body>
+                <table>
+                    <caption>Pokedex</caption>
+                    <thead>
+                        <tr>
+                            <th> Id </th>
+                            <th> Nome </th>
+                            <th> PvMax </th>
+                            <th> Ataque </th>
+                            <th> Defesa </th>
+                            <th> Ataque Especial </th>
+                            <th> Defesa Especial </th>
+                            <th> Velocidade </th>
+                            <th> Mais detalhes </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    ${content}
+                    </tbody>
+                </table>
+            </body>
+        </html>
+    `);
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
