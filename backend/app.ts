157091530b38458e8f4ff3d8dd8850c218d089d6
Bruno Mateus <brunomateus@gmail.com>
2022-01-19 00:28:22 -0300

    Adding config to enable Cors

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { router as pokemonRouter } from './routes/pokemons';
 import { engine } from 'express-handlebars';
+import cors from 'cors';
 
 const app = express();
 const PORT = 8080;
@@ -13,13 +14,14 @@ app.engine('hbs', engine({
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
-app.use('/pokemons', pokemonRouter);
+app.use(cors({ origin: 'http://localhost:3000'}))
+app.use('/pokemons', pokemonRouter)
 
 
 app.get('/', (req, res) => {
-    res.redirect('/pokemons');
-});
+    res.redirect('/pokemons')
+})
 
 app.listen(8080, () => {
     console.log(`Server listening on http://localhost:${PORT} `)
-});
+})
